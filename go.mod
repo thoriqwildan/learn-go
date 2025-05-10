1689789fb6049f1aec1a23474255caa3c0aaf2ea
thoriqwildan <wildanthoriq14@gmail.com>
2025-05-10 23:07:18 +0700

    First comit on standard library

---
+++ go.mod
@@ -0,0 +1,3 @@
+module standard-library
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	firstName := "Thoriq"
+	lastName := "Wildan"
+
+	fmt.Printf("Hello, %s %s!\n", firstName, lastName)
+}
